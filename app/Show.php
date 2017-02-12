b2d5e472f9c67b4597ea1286554c24b44d356043
John Grubb <grubb@johnnygrubb.com>
2017-02-12 14:42:36 -0500

    filling out and wiring up models and relationships

--- app/Show.php
+++ app/Show.php
@@ -8,6 +8,15 @@ class Show extends Model
 {
 
     public function songs() {
-        return $this->belongsToMany(Song::class, 'versions');
+        return $this->belongsToMany(Song::class, 'versions')
+            ->withPivot('position', 'transition', 'song_notes', 'set_number');
+    }
+
+    public function venue() {
+        return $this->belongsTo(Venue::class);
+    }
+
+    public function users() {
+        return $this->belongsToMany(User::class, 'shows_users');
     }
 }
